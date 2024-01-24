0d2e9dd1a1cbf8a25f0d7af0c3eb37a63e7fc041
LanreAndero <>
2024-01-24 15:59:45 +0000

    change DEBUG to False

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = (
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-lanreandero-django-proje-3zqx6jley3.us2.codeanyapp.com',
